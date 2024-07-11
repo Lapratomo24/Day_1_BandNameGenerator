9930b3e48cf713dc4576ced659c034801da764f3
Aditya <lapratomo24@yahoo.com>
2024-07-11 20:46:32 +0200

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# band name generator'
+
+print("Let's pick the name of our band!")
+
+city = input("Where did you grow up?\n")
+
+pet = input("What is the name of your first pet?\n")
+
+print(f"The name of our band shall be The {city} {pet}s!")
+
